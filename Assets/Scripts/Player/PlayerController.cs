bdc9a1074a1a3efbe8447bf614002eef5bad975b
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-24 10:14:38 +0200

    Flying doggo

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Vector2 _moveInput;
+
+    public void OnMove(InputAction.CallbackContext context) => _moveInput = context.ReadValue<Vector2>();
+
+    private void Update()
+    {
+        var move = new Vector3(_moveInput.x, _moveInput.y, 0) * (moveSpeed * Time.deltaTime);
+        transform.position += move;
+    }
 }
