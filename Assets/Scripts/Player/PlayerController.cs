2a430155c44a5d10e57a18b9e8d26a04f5f53b65
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-24 13:26:43 +0200

    SMack

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public Animator animator;
     private Vector2 _moveInput;
+    private static readonly int Smack = Animator.StringToHash("Smack");
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void OnMove(InputAction.CallbackContext context) => _moveInput = context.ReadValue<Vector2>();
 
+    public void OnSmack() => animator.SetTrigger(Smack);
     private void Update()
     {
         var move = new Vector3(_moveInput.x, _moveInput.y, 0) * (moveSpeed * Time.deltaTime);
