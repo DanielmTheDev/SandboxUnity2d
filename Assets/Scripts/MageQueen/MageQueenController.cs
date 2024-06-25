d1fc92d4555f0898619bb711ce333be7d043ec6e
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-25 16:31:58 +0200

    Wrap hit in cooldown

--- Assets/Scripts/MageQueen/MageQueenController.cs
+++ Assets/Scripts/MageQueen/MageQueenController.cs
@@ -5,16 +5,16 @@ namespace MageQueen
     public class MageQueenController : MonoBehaviour, IHittable
     {
         public Animator animator;
+        private CooldownExecutor _getHitExecutor;
+
         private static readonly int GetHit = Animator.StringToHash("GetHit");
 
         private void Start()
         {
             animator = GetComponent<Animator>();
+            _getHitExecutor = new(1, () => animator.SetTrigger(GetHit));
         }
 
-        public void OnHit()
-        {
-            animator.SetTrigger(GetHit);
-        }
+        public void OnHit() => _getHitExecutor.Execute();
     }
 }
