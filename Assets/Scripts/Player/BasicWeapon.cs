62321248b845c3a40869d47aa54725d7a371e91f
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-25 15:46:44 +0200

    OnHit

--- Assets/Scripts/Player/BasicWeapon.cs
+++ Assets/Scripts/Player/BasicWeapon.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class BasicWeapon : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("in trigger");
         var hittable = other.GetComponent<IHittable>();
